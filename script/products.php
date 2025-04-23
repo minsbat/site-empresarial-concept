a38dd675fce8c3fc5eaa55596565da87a0746840
mins <minsbat@users.noreply.github.com>
2025-04-22 23:49:33 -0300

    php inject

---
+++ script/products.php
@@ -0,0 +1,17 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "products_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+?>
